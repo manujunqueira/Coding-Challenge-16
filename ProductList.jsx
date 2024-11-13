a98a49f1244839bdbe6299271188ed354627530e
manujunqueira <manuelajunqueira27@gmail.com>
2024-11-13 14:10:39 -0500

    Implement keys for each ProductItem in ProductList

--- ProductList.jsx
+++ ProductList.jsx
@@ -7,11 +7,15 @@ import ProductItem from "./ProductItem";
 // ProductList component that takes a list of products as a prop
 const ProductList = ({ products }) => {
   return (
+    
+// Task 5 - Implement Keys in the ProductList Component
+// This was implemented in Task 3 by using product.id as the key in ProductItem.
+
     <div>
       {/* map through the products array and render a ProductItem for each product */}
-      {products.map((product) => (
+      {products.map((product) => ( 
         <ProductItem 
-          key={product.id} // unique key for each product item
+          key={product.id} // unique key for each product item (ALSO TASK 5)
           product={product} // pass the product object as a prop to ProductItem
         />
       ))}

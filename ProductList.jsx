12e1dfd7c1c5c7dd9ae420d667f1ee5cb71d2c17
manujunqueira <manuelajunqueira27@gmail.com>
2024-11-13 14:04:20 -0500

    Create ProductList component to render each ProductItem

---
+++ ProductList.jsx
@@ -0,0 +1,20 @@
+// ProductList.jsx
+// importing the ProductItem component to display individual products
+import ProductItem from "./ProductItem";
+
+// ProductList component that takes a list of products as a prop
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      {/* map through the products array and render a ProductItem for each product */}
+      {products.map((product) => (
+        <ProductItem 
+          key={product.id} // unique key for each product item
+          product={product} // pass the product object as a prop to ProductItem
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
